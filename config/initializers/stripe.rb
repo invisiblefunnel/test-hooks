185a93e90b2c50c9d6231b0fe154efadf1c16810
Danny Whalen <daniel.r.whalen@gmail.com>
2014-01-18 20:54:46 -0800

    Update to stripe_event 1.1.0

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
 Stripe.api_key = ENV['STRIPE_API_KEY'] || 'fake-api-key-for-testing'
 
-StripeEvent.setup do
+StripeEvent.configure do |events|
   # Clearly you'll be doing something meaningful here...
   # but we're logging for explanatory purposes.
   #
   # Watch the logs while your tests run:
   #   $ tail -f log/test.log
   #
-  subscribe 'customer.created' do |event|
+  events.subscribe 'customer.created' do |event|
     Rails.logger.info '**************************************************'
     Rails.logger.info event
     Rails.logger.info '**************************************************'
